41522c512788a7a921739866dde30744b0ff1b3a
Kaushallrai <Kaushallrai03@gmail.com>
2024-11-30 07:54:58 +0545

    navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,33 @@
 import Link from "next/link";
 import React from "react";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
   return (
-    <nav>
-      <Link href="/">Logo</Link>
-      <ul>
-        <li>
-          <Link href="/"></Link>
-        </li>
-        <li>
-          <Link href="/issues"></Link>
-        </li>
+    <nav className="flex items-center h-12 px-5 mb-5 space-x-6 border-b">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="transition-colors text-zinc-500 hover:text-zinc-800"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
